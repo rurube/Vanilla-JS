ecf9cdd647c6ce62932db6e4df6ede3c089f72c8
rurube <suga160329@naver.com>
2024-03-06 06:17:44 +0000

    6.0 Quotes

---
+++ js/quotes.js
@@ -0,0 +1,47 @@
+const quotes = [
+  {
+    quote: "지금까지의 모든 좌절은 성공을 위하는 과정의 일부라고 생각해요",
+    author: "FAKER",
+  },
+  {
+    quote: "가장 예쁜 꽃은 우여곡절을 겪고 피는 꽃",
+    author: "ONER",
+  },
+  {
+    quote: "잠시 흔들리거나 방향을 잃을지라도 지지 않겠다는 믿음으로 나아가겠습니다",
+    author: "FAKER",
+  },
+  {
+    quote: "a",
+    author: "a",
+  },
+  {
+    quote: "a",
+    author: "a",
+  },
+  {
+    quote: "a",
+    author: "a",
+  },
+  {
+    quote: "a",
+    author: "a",
+  },
+  {
+    quote: "a",
+    author: "a",
+  },
+  {
+    quote: "a",
+    author: "a",
+  },
+  {
+    quote: "a",
+    author: "a",
+  },
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+console.log(quotes[0 - 9])
