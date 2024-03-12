5cb7ab080c4bebb402cb61e834c309c857a1e066
rurube <suga160329@naver.com>
2024-03-12 13:00:47 +0900

    7.0 Setup

--- js/quotes.js
+++ js/quotes.js
@@ -1,47 +1,50 @@
 const quotes = [
   {
-    quote: "지금까지의 모든 좌절은 성공을 위하는 과정의 일부라고 생각해요",
-    author: "FAKER",
+    quote: "준비하지 않은 자는 기회가 와도 소용없다",
+    author: "Alexis de Tocqueville",
   },
   {
-    quote: "가장 예쁜 꽃은 우여곡절을 겪고 피는 꽃",
-    author: "ONER",
+    quote: "내일이란 오늘의 다른 이름일 뿐",
+    author: "William Cuthbert Faulkner",
   },
   {
-    quote: "잠시 흔들리거나 방향을 잃을지라도 지지 않겠다는 믿음으로 나아가겠습니다",
-    author: "FAKER",
+    quote: "강인한 의지 없이는 뛰어난 재능도 없다",
+    author: "Honore de Balzac",
   },
   {
-    quote: "a",
-    author: "a",
+    quote: "불가능한 일을 해보는 것은 신나는 일이다",
+    author: "Walter Elias Walt Disney",
   },
   {
-    quote: "a",
-    author: "a",
+    quote: "할 수 있다고 믿는 사람은 결국 그렇게 된다",
+    author: "Charles de Gaulle",
   },
   {
-    quote: "a",
-    author: "a",
+    quote: "한번 포기하면 습관이 된다. 절대 포기하지 말아라.",
+    author: "Michael Jeffrey Jordan",
   },
   {
-    quote: "a",
-    author: "a",
+    quote: "성공은 꿈꾸고, 믿고, 대담해지고, 실천하는 데서 오는 것이다",
+    author: "John Dijulius III",
   },
   {
-    quote: "a",
-    author: "a",
+    quote: "삶은 당신이 만드는 것이다. 이전에도 그랬고 앞으로도 그럴 것이다.",
+    author: "Grandma Moses",
   },
   {
-    quote: "a",
-    author: "a",
+    quote: "행운의 여신은 용기 있는 자를 좋아한다",
+    author: "Virgil Aboloh",
   },
   {
-    quote: "a",
-    author: "a",
+    quote: "긴 인생은 충분히 좋지 않을 수도 있다, 그러나 좋은 인생은 충분히 길다.",
+    author: "Benjamin Farnklin",
   },
 ];
 
 const quote = document.querySelector("#quote span:first-child");
 const author = document.querySelector("#quote span:last-child");
 
-console.log(quotes[0 - 9])
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
---
+++ js/todo.js
@@ -0,0 +1,12 @@
+const toDoForm = document.querySelector("#todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.querySelector("#todo-list");
+
+function handleToDoSubmit(event){
+    event.preventDefault();
+    const newToDo = toDoInput.value;
+    toDoInput.value = "";
+    console.log(newToDo, toDoInput.value);
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
