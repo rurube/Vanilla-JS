6b0345e7bb251a5aedcc2e13bdbd8998fae139c7
rurube <suga160329@naver.com>
2024-03-13 13:30:03 +0900

    7.1 adding todos

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,19 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
 
+function paintToDo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event){
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
-    console.log(newToDo, toDoInput.value);
+    paintToDo(newToDo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
